9a589acd6737145b1531fc6570a6667d0ff0e72b
desiMover <brijesh.pandya@gmail.com>
2020-06-20 13:48:02 +0800

    Add project files.

---
+++ PhotoLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotoLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
